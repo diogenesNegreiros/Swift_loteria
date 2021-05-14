e557421fe557efe1287feb4d1d4ff6df87d642cf
Diogenes <diogenesdesouza@yahoo.com.br>
2021-05-13 22:57:32 -0300

    initial commit grafics interface

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Diogenes de Souza on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbGameTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
