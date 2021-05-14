5db6f410bf67c655f7a3f4c66c043b4a55ded72c
Diogenes <diogenesdesouza@yahoo.com.br>
2021-05-14 09:58:25 -0300

    manager game controller

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -7,6 +7,24 @@
 
 import UIKit
 
+enum GameType: String {
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
+infix operator >-<
+func >-< (total: Int, universe: Int) -> [Int] {
+    var result: [Int] = []
+    while result.count < total {
+        let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        if !result.contains(randomNumber) {
+            result.append(randomNumber)
+        }
+    }
+    return result.sorted()
+}
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var lbGameTitle: UILabel!
@@ -16,10 +34,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showNumbers(for: .megasena)
     }
 
     @IBAction func generateGame() {
+
+        switch segmentedGameType.selectedSegmentIndex {
+        case 0:
+            showNumbers(for: .megasena)
+            
+        default:
+            showNumbers(for: .quina)
+
+        }
+    
+    }
+    
+    func showNumbers(for type: GameType) {
+        lbGameTitle.text = type.rawValue
+        var game: [Int] = []
+        
+        switch type {
+        case .megasena:
+            game = 6>-<60
+            balls.last!.isHidden = false
+            print("mega-sena")
+            
+        case .quina:
+            game = 5>-<80
+            balls.last!.isHidden = true
+
+            print("quina")
+        }
+        for (index, game) in game.enumerated() {
+            balls[index].setTitle("\(game)", for: .normal)
+        }
     }
     
 }
